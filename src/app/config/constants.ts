a86a7420091575fd89f374ee4ecf1dfe098000f9
Mauricio Carvalho <developer.mauricio1@gmail.com>
2025-02-08 21:58:35 -0300

    fix: url

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,3 +1,3 @@
 import 'dotenv/config'
 console.log(process.env)
-export const API_URL = process.env.API_URL ?? 'http://restaurante-acme-video-svc.fiap.svc.cluster.local'
+export const API_URL = process.env.API_URL ?? 'http://a5d905fae06924cbfae9f21f9712b77f-765655578.us-east-1.elb.amazonaws.com'
